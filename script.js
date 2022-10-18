ba1450362cf9834aaad488de86e5eedf0b6a4cc1
Ogaga <iyaraogaga69@gmail.com>
2022-10-18 16:55:48 +0100

    Select all neccessary elements

---
+++ script.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+'use strict'
+
+const nav = document.querySelector('.nav')
+const navLinks = document.querySelector('.nav__links')
+const date = document.querySelector('.date')
+const bookList = document.querySelector('.book-list')
+const removeBtn = document.querySelectorAll('.remove')
+const titleInput = document.querySelector('.title')
+const authorInput = document.querySelector('.author')
+const addBtn = document.querySelector('.add')
+
+
+
