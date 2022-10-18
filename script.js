ac84d4c5ad3eaf5907aac7419e879bfa4bd76f49
Ogaga <iyaraogaga69@gmail.com>
2022-10-18 17:21:04 +0100

    Add event listener for adding books

--- script.js
+++ script.js
@@ -10,6 +10,43 @@ const removeBtn = document.querySelectorAll('.remove')
 const titleInput = document.querySelector('.title')
 const authorInput = document.querySelector('.author')
 const addBtn = document.querySelector('.add')
+const books = []
 
+class Book {
+  date = new Date();
+  id = (Date.now() + "").slice(-10);
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
 
+const renderBook = (book) => {
+    const html = `
+        <li class="list-item" data-id=${book.id}>
+                    <p class="list-para">
+                        <span class="title">${book.title}</span>
+                        by
+                        <span class="author">${book.author}</span>
+                    </p>
+                    <button class="submit" type="button">Remove</button>
+                </li>
+    `;
+    bookList.insertAdjacentHTML('afterbegin', html)
+}
+
+addBtn.addEventListener('click', () => {
+    const author = authorInput.value
+    const title = titleInput.value
+    let book;
+
+    if (!author || !title) return;
+
+    book = new Book(title, author)
+    renderBook(book)
+    books.push(book)
+    console.log(books)
+
+    authorInput.value = titleInput.value = ''
+})
 
